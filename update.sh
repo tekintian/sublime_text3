c7f5d3d7b762e9eaa2972a89219217f5c1aeccb6
myhkserver <tekin@vip.qq.com>
2020-09-19 14:24:47 +0800

    update channel_v3 20200919T14%i1600496677

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
+DATETIME=$(date +"%Y%m%dT%H%i%s")
+
 rm -f ${SHELL_FOLDER}/.git/index.lock
 # change workdir to git
 echo "go into ${SHELL_FOLDER}"
@@ -17,7 +19,7 @@ curl -o ${SHELL_FOLDER}/channel_v3.json https://packagecontrol.io/channel_v3.jso
 echo "ready to update remote"
 git status
 git add channel_v3.json
-git commit -m "update channel_v3"
+git commit -m "update channel_v3 ${DATETIME}"
 git push
 
 echo "done"
